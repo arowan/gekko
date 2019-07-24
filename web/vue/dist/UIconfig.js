b50869e4d78e8dfb9a25a1da250d62dd17ba39f5
Alex Rowan <arowanuk@gmail.com>
2019-07-24 20:58:53 +0100

    ui

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -1,28 +1,9 @@
-// SERVER
+// Note: this file gets copied around, make sure you edit
+// the UIconfig located at `gekko/web/vue/dist/UIconfig.js`.
 
-// const CONFIG = {
-//   headless: true,
-//   api: {
-//     host: '127.0.0.1',
-//     port: 3000,
-//   },
-//   ui: {
-//     ssl: true,
-//     host: 'candlestick.alexrowan.net',
-//     port: 443,
-//     path: '/'
-//   },
-//   adapter: 'sqlite'
-// }
-//
-// if(typeof window === 'undefined')
-//   module.exports = CONFIG;
-// else
-//   window.CONFIG = CONFIG;
+// This config is used by both the frontend as well as the web server.
+// see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
-
-
-// DEV
 const CONFIG = {
   headless: false,
   api: {
